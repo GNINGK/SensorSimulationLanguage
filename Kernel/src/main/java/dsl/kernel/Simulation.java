caa8613c9500b88d054d8e3920492cd575f11b5c
Anthonny Giroud <giroud.anthonny@gmail.com>
2018-02-07 10:43:08 +0100

    Added LOGGER in Simulation

--- Kernel/src/main/java/dsl/kernel/Simulation.java
+++ Kernel/src/main/java/dsl/kernel/Simulation.java
@@ -8,6 +8,8 @@ package main.java.dsl.kernel;
 import main.java.dsl.kernel.definition.Tuple;
 import main.java.dsl.kernel.structure.Capteur;
 import main.java.dsl.kernel.structure.Lieu;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -15,16 +17,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * @author Maxime
  */
 public class Simulation implements NamedElement {
 
+    private static final Logger LOGGER = Logger.getLogger(Simulation.class);
+
     private String name;
     private List<Lieu> lieux;
     private int tempsTotalSimulation;
 
-    public Simulation(int tempsTotalSimulation) {
+    Simulation(int tempsTotalSimulation) {
+        BasicConfigurator.configure();
+
         this.tempsTotalSimulation = tempsTotalSimulation;
         lieux = new ArrayList<>();
     }
@@ -66,31 +73,40 @@ public class Simulation implements NamedElement {
     public void run() {
         for (Lieu l : lieux) {
             List<Capteur> listCapteur = l.getCapteurs();
-            //List<Tuple> resultat = new ArrayList<>();
-            System.out.println("Lieu : " + l.getName());
+            List<Tuple> resultat = new ArrayList<>();
+            LOGGER.info("Lieu : " + l.getName());
             for (Capteur c : listCapteur) {
-                System.out.println("capteur : " + c.getName());
+                LOGGER.info("capteur : " + c.getName());
+
                 for (int i = 0; i < tempsTotalSimulation; i++) {
                     if (i % c.getEchantillonnage() == 0) {
-                        System.out.println(c.generationDonnees(i));
+                        LOGGER.info(c.generationDonnees(i));
+                        resultat.add(new Tuple(i, c.getName(), c.generationDonnees(i)));
                     }
-
                 }
             }
-            //sauvegardeCSV(resultat, "");
+            sauvegardeCSV(resultat, "resultat.csv");
         }
     }
 
-    private void sauvegardeCSV(List<Tuple> resultat, String pathOutput) throws IOException {
-        File file = null;
+    private void sauvegardeCSV(List<Tuple> resultat, String pathOutput) {
         if (pathOutput.isEmpty()) {
-            System.out.println("Erreur : Il n'y a pas de chemin pour le fichier");
-        } else {
-            file = new File(pathOutput);
+            LOGGER.error("Attention : Il n'y a pas de chemin pour le fichier");
+            return;
         }
+
+        File file = new File(pathOutput);
+
         if (!file.exists()) {
-            file.createNewFile();
+            try {
+                if (!file.createNewFile()) {
+                    LOGGER.error("Erreur : Il n'y a pas de chemin pour le fichier");
+                }
+            } catch (IOException e) {
+                LOGGER.error(e);
+            }
         }
+
         try (FileWriter writer = new FileWriter(file)) {
             for (int i = 0; i < resultat.size(); i++) {
                 if (i == resultat.size() - 1) {
@@ -103,7 +119,7 @@ public class Simulation implements NamedElement {
             }
             writer.flush();
         } catch (IOException e) {
-            System.out.println("Erreur: impossible de créer le fichier '" + pathOutput + "'");
+            LOGGER.error("Erreur: impossible de créer le fichier '" + pathOutput + "'");
         }
     }
 
