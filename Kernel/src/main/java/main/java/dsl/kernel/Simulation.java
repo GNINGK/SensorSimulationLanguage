9b12d0c421dc24d04078997b5e2200df30ca685e
Anthonny Giroud <giroud.anthonny@gmail.com>
2018-02-19 08:54:31 +0100

    Undo comments

--- Kernel/src/main/java/main/java/dsl/kernel/Simulation.java
+++ Kernel/src/main/java/main/java/dsl/kernel/Simulation.java
@@ -33,14 +33,14 @@ public class Simulation implements NamedElement {
 
     public Simulation() {
         BasicConfigurator.configure();
-        //initDB("http://localhost:8086", "sensors_database");
+        initDB("http://localhost:8086", "sensors_database");
 
         this.places = new ArrayList<>();
     }
 
     public Simulation(int totalTime) {
         BasicConfigurator.configure();
-        //initDB("http://localhost:8086", "sensors_database");
+        initDB("http://localhost:8086", "sensors_database");
         this.places = new ArrayList<>();
 
         this.totalTime = totalTime;
@@ -102,7 +102,7 @@ public class Simulation implements NamedElement {
                 for (int i = 0; i < this.totalTime; i++) {
                     if (i % sensor.getEchantillonnage() == 0) {
                         long t = System.currentTimeMillis() - this.totalTime * 1000 + i * 1000;
-                        //saveToDB(new Tuple(t, sensor.getName(), sensor.generationDonnees(i)));
+                        saveToDB(new Tuple(t, sensor.getName(), sensor.generationDonnees(i)));
                     }
                 }
             }
