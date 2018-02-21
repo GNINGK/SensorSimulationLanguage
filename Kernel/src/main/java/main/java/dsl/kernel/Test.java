06ee53c4c2df3ba6cd25670762f9047936e158a9
Anthonny Giroud <giroud.anthonny@gmail.com>
2018-02-21 12:33:43 +0100

    Updated groovy script & Main

--- Kernel/src/main/java/main/java/dsl/kernel/Test.java
+++ Kernel/src/main/java/main/java/dsl/kernel/Test.java
@@ -11,40 +11,45 @@ public class Test {
 
     public static void main(String[] args) {
 
+        // Définitions des batiments
+        Place batA = new Place("batA");
+
+        // Définitions des fonctions
         Functions function1 = new Polynomial(new Double[]{0.0, 2.0, -1.0 / 10.0}, 0, 20);
         Functions function2 = new Polynomial(new Double[]{0.0, 2.0, -1.0 / 5.0}, 0, 10);
+
+        batA.addSensor(new Sensor("sensor1", function1, 1));
+        batA.addSensor(new Sensor("sensor2", function2, 1));
+
         Functions function3 = new Polynomial(new Double[]{10.0, 0.0, -1.0 / 10.0}, -10, 10);
 
-        IntervalFunctions ifs = new IntervalFunctions();
+        IntervalFunctions function_interval = new IntervalFunctions();
         Interval i = new Interval(0.0, 40.0);
         Interval i2 = new Interval(41.0, 60.0);
         Interval i3 = new Interval(61.0, 100.0);
-        ifs.add(i, function1);
-        ifs.add(i2, function2);
-        ifs.add(i3, function3);
+        function_interval.add(i, function1);
+        function_interval.add(i2, function2);
+        function_interval.add(i3, function3);
 
-        CSVLoader csvLoader = new CSVLoader("dataSource.csv", "sensor0", 0, 10);
+        batA.addSensor(new Sensor("sensor_interval", function_interval, 1));
+
+        // CSV
+        CSVLoader function_csv = new CSVLoader("dataSource.csv", "sensor0", 0, 10);
+
+        batA.addSensor(new Sensor("sensor_csv", function_csv, 1));
+
+        // MARKOV
         int nbEtat = 2;
         String[] nameEtat = {"Sunny", "Rainy"};
         float[][] transition = {{0.1f, 0.9f}, {0.5f, 0.5f}};
-        Markov markovLaw = new Markov(nbEtat, 0, nameEtat, transition);
-        markovLaw.setFrequence(5);
+        Markov function_markov = new Markov(nbEtat, 0, nameEtat, transition);
+        function_markov.setFrequence(5);
 
-        //Definitions des batiments
-        Place batA = new Place("batA");
+        batA.addSensor(new Sensor("sensor_markov", function_markov, 1));
 
         //Definition de la simulation
         Simulation simulation = new Simulation(100);
-
-        batA.addSensor(new Sensor<>("sensor_markov", markovLaw, 1));
-
-        batA.addSensor(new Sensor("sensor_csv", csvLoader, 1));
-        batA.addSensor(new Sensor("sensor1", function1, 1));
-        batA.addSensor(new Sensor("sensor2", function2, 1));
-        batA.addSensor(new Sensor("sensor_interval", ifs, 1));
-
         simulation.addPlaces(batA);
-
         simulation.run();
     }
 
